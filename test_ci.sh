978f0cdf5e79897dd234b3d0adf7c3201b484f83
ZonobiaX <55743424+ZonobiaX@users.noreply.github.com>
2024-04-06 16:19:18 +0800

    Update test_ci.sh

--- test_ci.sh
+++ test_ci.sh
@@ -5,7 +5,7 @@ pip install -r requirements.txt
 
 FAIL_LIMIT=3
 
-for plugin in "torch_ddp" "torch_ddp_fp16" "gemini" "low_level_zero" "hybrid_parallel"; do
+for plugin in "torch_ddp" "torch_ddp_fp16" "gemini" "low_level_zero"; do
     for i in $(seq 1 $FAIL_LIMIT); do
         torchrun --standalone --nproc_per_node 1 finetune.py --target_f1 0.8 --plugin $plugin --model_type "bert" && break
         echo "Failed $i times"
