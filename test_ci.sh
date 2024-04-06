1accbb79999e5305b4db697526bde050dc95c021
ZonobiaX <55743424+ZonobiaX@users.noreply.github.com>
2024-04-06 16:04:40 +0800

    Add files via upload

---
+++ test_ci.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+
+pip install -r requirements.txt
+
+FAIL_LIMIT=3
+
+for plugin in "torch_ddp" "torch_ddp_fp16" "gemini" "low_level_zero" "hybrid_parallel"; do
+    for i in $(seq 1 $FAIL_LIMIT); do
+        torchrun --standalone --nproc_per_node 1 finetune.py --target_f1 0.8 --plugin $plugin --model_type "bert" && break
+        echo "Failed $i times"
+        if [ $i -eq $FAIL_LIMIT ]; then
+            echo "Failed $FAIL_LIMIT times, exiting"
+            exit 1
+        fi
+    done
+done
